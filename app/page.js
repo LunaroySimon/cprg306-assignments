a2e84e20339fbe920a8c3ea531b67671f83f1766
Lunaroy13 <romano_luna@hotmail.com>
2024-10-01 06:50:10 -0600

    Doing optional step for assignment 3 - changing the title of the metaddata object and adding styling to main page

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 export default function Page() {
     return (
-      <main>
-        <h1>CPRG 306: Web Development 2 - Assignments</h1>
-        <p><Link href="/week-2">Week 2 Assignment Link</Link></p>
-        <p><Link href="/week-3">Week 3 Assignment Link</Link></p>
+      <main className="m-10">
+        <h1 className="text-4xl m-10">CPRG 306: Web Development 2 - Assignments</h1>
+        <p className="text-2xl p-2 hover:text-cyan-600"><Link href="/week-2">Week 2 Assignment Link</Link></p>
+        <p className="text-2xl p-2 hover:text-cyan-600"><Link href="/week-3">Week 3 Assignment Link</Link></p>
       </main>
     );
   }
